37dc8d3bb5b435ef215066f6696b88f4e227f7f1
leon mangu <leon.mangu@gmail.com>
2020-04-24 16:22:20 +0300

    Add static page rendering

--- myclub_root/events/views.py
+++ myclub_root/events/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import date
+
 
 # Create your views here.
+
+def index(request):
+    
+    return HttpResponse("<h2>Myclub Event Calendar</h2>")
