cafe64a1d7d49e8d5356b5bf6fbcabecf1b73a15
leon mangu <leon.mangu@gmail.com>
2020-04-24 16:24:36 +0300

    Add dynamic page rendering

--- myclub_root/events/views.py
+++ myclub_root/events/views.py
@@ -6,5 +6,9 @@ from datetime import date
 # Create your views here.
 
 def index(request):
-    
-    return HttpResponse("<h2>Myclub Event Calendar</h2>")
+    t = date.today()
+    month = date.strftime(t, '%b')
+    year = t.year
+    title = "MyClub Event Calendar - {} {}".format(month, year)
+    return HttpResponse("<h2>{}</h2>".format(title))
+    # return HttpResponse("<h2>Myclub Event Calendar</h2>")
