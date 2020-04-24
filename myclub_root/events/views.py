53fe6a8f23d8ac3b4ba7bd85ba89ce6adc9ed6f1
leon mangu <leon.mangu@gmail.com>
2020-04-24 16:54:28 +0300

    Add HTML calendar object

--- myclub_root/events/views.py
+++ myclub_root/events/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import date
+import calendar
+from calendar import HTMLCalendar
 
 
 # Create your views here.
 
-def index(request):
-    t = date.today()
-    month = date.strftime(t, '%b')
-    year = t.year
-    title = "MyClub Event Calendar - {} {}".format(month, year)
-    return HttpResponse("<h2>{}</h2>".format(title))
+def index(request, year, month):
+    # t = date.today()
+    # month = date.strftime(t, '%b')
+    # year = t.year
+
+    year = int(year)
+    month = int(month)
+
+    if year < 2000 or year > 2099:
+        year = date.today().year
+        month = date.today().month
+    month_name = calendar.month_name[month]
+    title = "MyClub Event Calendar - {} {}".format(month_name, year)
+    # return HttpResponse("<h2>{}</h2>".format(title))
     # return HttpResponse("<h2>Myclub Event Calendar</h2>")
+    cal = HTMLCalendar().formatmonth(year, month)
+    return HttpResponse("<h2>{}</h2><p>{}</p>".format(title, cal))
